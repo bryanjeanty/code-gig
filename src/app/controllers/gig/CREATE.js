d0a14886dad764657d0e3cb06948df9f386c06ae
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-04-14 03:58:13 -0400

    add gigs form has dynamic functionality

--- src/app/controllers/gig/CREATE.js
+++ src/app/controllers/gig/CREATE.js
@@ -5,27 +5,54 @@ const Gig = require("../../models/gig.js");
 
 // Post a gig
 const CREATE = (req, res, next) => {
-  const data = {
-    title: "Simple Wordpress Website",
-    technologies: "Wordpress, PHP, HTML, CSS",
-    budget: "$1000",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-    contact_email: "user2@gmail.com"
-  };
+  let { title, technologies, budget, description, contact_email } = req.body;
+  let errors = [];
 
-  let { title, technologies, budget, description, contact_email } = data;
+  // Validate Fields
+  if (!title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!technologies) {
+    errors.push({ text: "Please add some technologies" });
+  }
+  if (!description) {
+    errors.push({ text: "Please add a description" });
+  }
+  if (!contact_email) {
+    errors.push({ text: "Please add a contact email" });
+  }
 
-  // Insert into table
-  Gig.create({
-    title,
-    technologies,
-    budget,
-    description,
-    contact_email
-  })
-    .then(gig => res.redirect("/gigs"))
-    .catch(error => console.error("error", error));
+  // Check for errors
+  if (errors.length > 0) {
+    res.render("add", {
+      errors,
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email
+    });
+  } else {
+    if (!budget) {
+      budget = "Unknown";
+    } else {
+      budget = `$${budget}`;
+    }
+
+    // Make lowercase and remove space after comma
+    technologies = technologies.toLowerCase().replace(/, /g, ",");
+
+    // Insert into table
+    Gig.create({
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email
+    })
+      .then(gig => res.redirect("/gigs"))
+      .catch(error => console.error("error", error));
+  }
 };
 
 module.exports = CREATE;
